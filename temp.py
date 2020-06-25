d2b58c1386b6f46b005a40cc9e4779d9b2ef0b15
imtarit <imtarit@gmail.com>
2020-06-25 14:50:51 +1000

    Update temp.py

--- temp.py
+++ temp.py
@@ -19,77 +19,43 @@ voxels = voxels[..., np.newaxis]
 # The inputs are 50x50x50 volumes with a single channel, and the
 # batch size is 100
 
-# def build_model():
-#   model = keras.Sequential([
-#     layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
-#     layers.Dense(64, activation='relu'),
-#     layers.Dense(1)
-#   ])
-  
-
-# input_shape =(100, 50, 50, 50, 1)
-# x = tf.random.normal(input_shape)
-# print(type(x))
-
-# tf.keras.layers.Conv3D(
-#     filters, kernel_size, strides=(1, 1, 1), padding='valid', data_format=None,
-#     dilation_rate=(1, 1, 1), activation=None, use_bias=True,
-#     kernel_initializer='glorot_uniform', bias_initializer='zeros',
-#     kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
-#     kernel_constraint=None, bias_constraint=None, **kwargs
-# )
-
-# def autoencoder_3D(inputShape = [100,50,50,1]):
-#   model = tf.keras.models.Sequential([
-#     tf.keras.layers.Conv3D(2, 3, activation='relu', input_shape=inputShape),
-#     tf.keras.layers.Conv3DTranspose(1,3,activation='relu')
-#   ])
-#   model.compile(optimizer='adam',
-#               loss=tf.losses.BinaryCrossentropy(from_logits=True),
-#               metrics=['accuracy'])
-#   return model
-
-# layer_1 = tf.keras.layers.Conv3D(2, 3, activation='relu', input_shape=voxels.shape)
-# y = layer_1(voxels.astype(np.float32))
-# print(y.shape)
-
-# tf.keras.layers.Conv3DTranspose(
-#     filters, kernel_size, strides=(1, 1, 1), padding='valid', output_padding=None,
-#     data_format=None, activation=None, use_bias=True,
-#     kernel_initializer='glorot_uniform', bias_initializer='zeros',
-#     kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
-#     kernel_constraint=None, bias_constraint=None, **kwargs
-# # )
-# layer_2 = tf.keras.layers.Conv3DTranspose(1,3,activation='relu')
-# z = layer_2(y)
-# print(z.shape)
-
-# model = autoencoder_3D([60,20,20,1])
-# model.compile(optimizer=tf.keras.optimizers.Adam(1e-3), loss=tf.keras.losses())
 
 inputShape = [50, 50, 50, 1]
 latent_dim = 3
 model = keras.Sequential([
     keras.layers.Conv3D(2, 3, activation='relu', input_shape=inputShape),
-    keras.layers.Conv3D(4, 3, activation='relu'),
+    keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool3D(pool_size=(3, 3, 3)),
+    keras.layers.Conv3D(4, 5, activation='relu'),
+    keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2)),
     keras.layers.Conv3D(8, 3, activation='relu'),
+    keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2)),
     keras.layers.Flatten(),
     keras.layers.Dense(latent_dim),
-    keras.layers.Dense(44*44*44*8),
-    keras.layers.Reshape(target_shape = (44,44,44,8)),
+    keras.layers.Dense(2*2*2*8),
+    keras.layers.BatchNormalization(),
+    keras.layers.Reshape(target_shape = (2,2,2,8)),
+    tf.keras.layers.UpSampling3D(size=(2, 2, 2)),
     keras.layers.Conv3DTranspose(4,3,activation='relu'),
-    keras.layers.Conv3DTranspose(2,3,activation='relu'),
+    keras.layers.BatchNormalization(),
+    tf.keras.layers.UpSampling3D(size=(2, 2, 2)),
+    keras.layers.Conv3DTranspose(8,5,activation='relu'),
+    keras.layers.BatchNormalization(),
+    tf.keras.layers.UpSampling3D(size=(3, 3, 3)),
     keras.layers.Conv3DTranspose(1,3,activation='relu')
+    
 ])
-model.compile(optimizer=keras.optimizers.Adam(1e-3), loss='MSE')
+model.compile(optimizer=keras.optimizers.Adam(1e-4), loss='MSE')
 print(model.summary())
-print(voxels.shape)
+# print(voxels.shape)
 
-history = model.fit(voxels, voxels, epochs=10)
+history = model.fit(voxels, voxels, epochs=1000)
 
 plt.plot(history.history['loss'], label='loss')
 # plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-# plt.ylim([0.5, 1])
+plt.ylabel('Loss')
+# # plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
